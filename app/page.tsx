273264f92e903f58ba963e05343bc610c5bc3762
mauricebarksdale <momobarksdale@gmail.com>
2024-07-25 11:09:12 -0400

    Changed home description

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Maurice Barksdale
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Hello! I'm a software engineer who studies Computer Science at the University of
+        Maryland College Park! I get bored very easily, so I delve into full stack development, ! `}
       </p>
       <div className="my-8">
         <BlogPosts />
